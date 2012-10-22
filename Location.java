ec7cd58a0202f5b8c4649cd98868f4412aceb1a7
Morka <Nief@mx.at>
2012-10-22 21:08:42 +0200

    ...

--- Location.java
+++ Location.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+
 public class Location{
 	
-		public Location(){
+	private HashMap<String, String> placeDescriptions;
+	
+	public Location(){
+		placeDescriptions = new HashMap<String, String>();
+	}
+	
+	public void addDescription(String nameOfDescription, String description){
+		nameOfDescription = nameOfDescription.toLowerCase();
 		
+		placeDescriptions.put(nameOfDescription, description);			
+	}
+	
+	public String searchDescription(String nameOfDescription){
+		String description = "Not Found";
+		nameOfDescription = nameOfDescription.toLowerCase();
+		
+		
+		if(placeDescriptions.containsKey(nameOfDescription)){
+			description = placeDescriptions.get(nameOfDescription);
 		}
+		
+		return description;
+	}
 }
