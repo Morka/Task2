2ede095272c2f2df47ffea5e836a642b72422417
Morka <Nief@mx.at>
2012-10-23 12:33:24 +0200

    Location update

--- Location.java
+++ Location.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Location{
 	
@@ -24,5 +25,21 @@ public class Location{
 		}
 		
 		return description;
+	}
+	
+	public boolean neededInfrastructure(ArrayList<String> neededInfrastracture){
+		boolean hasEverything = true;
+		
+		if(neededInfrastracture == null){
+			return false;
+		}
+		
+		for(String s : neededInfrastracture){
+			if(!placeDescriptions.containsKey(s.toLowerCase){
+				hasEverything = false;	
+			}					
+		}
+		
+		return hasEverything;
 	}
 }
